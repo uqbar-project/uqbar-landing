664a758389f966b6b04622a40ea698b780335b2b
Ernesto Bossi <bossi.ernestog@gmail.com>
2019-10-17 11:54:10 -0300

    Updates

---
+++ precache-manifest.3a01eaf9f8e4f115296cd08cd453a81c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc2264c9ee10c708338c7c2a06b434a",
+    "url": "/uqbar-landing/index.html"
+  },
+  {
+    "revision": "e31eea7b685904854e52",
+    "url": "/uqbar-landing/static/css/main.d3677bb7.chunk.css"
+  },
+  {
+    "revision": "eb7c91b79349240fb894",
+    "url": "/uqbar-landing/static/js/2.3dbb6dbf.chunk.js"
+  },
+  {
+    "revision": "e31eea7b685904854e52",
+    "url": "/uqbar-landing/static/js/main.fcdcceae.chunk.js"
+  },
+  {
+    "revision": "495cafa910517d84a64a",
+    "url": "/uqbar-landing/static/js/runtime-main.d6842fb7.js"
+  },
+  {
+    "revision": "f119eb5fed184049df062aef94690771",
+    "url": "/uqbar-landing/static/media/demo.f119eb5f.jpg"
+  },
+  {
+    "revision": "b5dd20af564fb1efaab1f93ca4f61a6b",
+    "url": "/uqbar-landing/static/media/ilustracion01.b5dd20af.svg"
+  },
+  {
+    "revision": "754ea763035ce43cdfbf2187f070f61e",
+    "url": "/uqbar-landing/static/media/ilustracion02.754ea763.svg"
+  },
+  {
+    "revision": "d3a0c840c5fe67ad683eccfe1bff21c8",
+    "url": "/uqbar-landing/static/media/ilustracion03.d3a0c840.svg"
+  },
+  {
+    "revision": "4f2a531d62e75f01d5d2ca9b08709464",
+    "url": "/uqbar-landing/static/media/logo.4f2a531d.svg"
+  },
+  {
+    "revision": "bb0623b8bcc061ced1b845a09fc21fc5",
+    "url": "/uqbar-landing/static/media/mercadopago.bb0623b8.svg"
+  },
+  {
+    "revision": "f8889df2ffd57b182221e1165903ef8b",
+    "url": "/uqbar-landing/static/media/proyecto-arena.f8889df2.png"
+  },
+  {
+    "revision": "a613c25a6f815c0b306f5613c09b9ac2",
+    "url": "/uqbar-landing/static/media/proyecto-uqbarwiki.a613c25a.png"
+  },
+  {
+    "revision": "8f5c1e0f80fe4e34f05a2a10955e8c16",
+    "url": "/uqbar-landing/static/media/proyecto-wollok.8f5c1e0f.png"
+  },
+  {
+    "revision": "4a4e30bf7947eda1eb2ccff416c9d636",
+    "url": "/uqbar-landing/static/media/proyecto-yanapiri.4a4e30bf.png"
+  },
+  {
+    "revision": "5fa810a9261a32a5f039f408993e79be",
+    "url": "/uqbar-landing/static/media/red-email.5fa810a9.svg"
+  },
+  {
+    "revision": "ce348692a2f333b03229941955abc9ff",
+    "url": "/uqbar-landing/static/media/red-facebook.ce348692.svg"
+  },
+  {
+    "revision": "c3371bf6765f51d6c8cce98352124250",
+    "url": "/uqbar-landing/static/media/red-instagram.c3371bf6.svg"
+  },
+  {
+    "revision": "a7112ae0399eb5955198e31d0d1686c4",
+    "url": "/uqbar-landing/static/media/red-twitter.a7112ae0.svg"
+  },
+  {
+    "revision": "ab92d4b3f117d69526de636e8818e957",
+    "url": "/uqbar-landing/static/media/red-youtube.ab92d4b3.svg"
+  },
+  {
+    "revision": "1efa9df16c349b5c063820ff606e1ab8",
+    "url": "/uqbar-landing/static/media/valores01.1efa9df1.svg"
+  },
+  {
+    "revision": "8514c01bf9d49a3093efcd402467e0ea",
+    "url": "/uqbar-landing/static/media/valores02.8514c01b.svg"
+  },
+  {
+    "revision": "86316264cca37b24dce27fa73ab9adb2",
+    "url": "/uqbar-landing/static/media/valores03.86316264.svg"
+  },
+  {
+    "revision": "9643593f7994aff532c3c2d3053dbd7b",
+    "url": "/uqbar-landing/static/media/valores04.9643593f.svg"
+  }
+]);
