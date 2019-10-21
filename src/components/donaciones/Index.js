d491c78334163fa8afc9af9b83444774fca235a8
Agustín Ruiz <hola@agustinruiz.com>
2019-10-21 10:30:25 -0300

    Links de mercadopago

--- src/components/donaciones/Index.js
+++ src/components/donaciones/Index.js
@@ -12,7 +12,8 @@ export default function Donaciones() {
             <h2>Donaciones</h2>
             <p className={styles["destacado"]}>
               <strong>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. </strong>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+              </strong>
               Donec volutpat elit sit amet auctor tempus. Vivamus dapibus lectus
               at purus euismod volutpat. Ut aliquet massa nec dolor iaculis
               rutrum.
@@ -23,11 +24,43 @@ export default function Donaciones() {
               links:
             </p>
             <div className={styles["linksMercadoPago"]}>
-              <a href="#">$100</a>
-              <a href="#">$200</a>
-              <a href="#">$500</a>
-              <a href="#">$1000</a>
-              <a href="#">Otro monto</a>
+              <a
+                mp-mode="dftl"
+                href="https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=459677732-ca60b8ed-ba7c-4675-82cd-f2276b7c0b27"
+                name="MP-payButton"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                $100
+              </a>
+              <a
+                mp-mode="dftl"
+                href="https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=459677732-892b033e-e35b-4863-bcf1-46ebd5f92ed1"
+                name="MP-payButton"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                $200
+              </a>
+              <a
+                mp-mode="dftl"
+                href="https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=459677732-b4aba3ee-b849-4440-9e3d-9f6649cea277"
+                name="MP-payButton"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                $500
+              </a>
+              <a
+                mp-mode="dftl"
+                href="https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=459677732-830f85e7-13bb-499a-97c6-1cd1c0c75cbc"
+                name="MP-payButton"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                $1000
+              </a>
+              
             </div>
             <div className={styles["logoMercadoPago"]}>
               <img src={mercadopago} alt="Mercado Pago" />
