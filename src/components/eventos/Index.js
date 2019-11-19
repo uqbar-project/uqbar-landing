cd89a165d3b3217f35fbd5a85ccd0a6d91c4228f
PalumboN <nahuel.palumbo@gmail.com>
2019-11-19 18:53:26 -0300

    Actualizo eventos

--- src/components/eventos/Index.js
+++ src/components/eventos/Index.js
@@ -7,18 +7,22 @@ import jornadasDocentes from "./jornadas-docentes.jpg";
 export default function Eventos() {
   return (
     <div className={styles["eventos"]} id="Eventos">
+      <h2>Próximos</h2>
+      <div className={styles["eventosListado"]}>
+        <UqbarCard
+          link="https://docs.google.com/document/d/1hIyF7JHVLJ2u-a69tJRH5YfxPIdaiMDav3tCArkX-qc/view"
+          projectLogo={concursoWollokGame}
+          projectName="Concurso Wollok Game"
+          projectDescription="Si hiciste o estás haciendo un juego con Wollok Game, si estás aprendiendo a programar en objetos con Wollok y tenés ganas de explorar la interfaz gráfica de Game, si sos docente y queres mantenerte actualizado con las herramientas de Wollok, ¡este concurso es para vos! Junto con tu grupo, o individualmente, pueden hacer el juego que prefieran, presentarlo, que sea analizado por el jurado, que el público dé su opinión y ganar premios."
+        />
+      </div>
+      <h2>Pasados</h2>
       <div className={styles["eventosListado"]}>
         <UqbarCard
           link="https://docs.google.com/document/d/1rJdrULtkcoLSuSzcp98ishrMS_04xPBdVW0Gyp3llno/edit"
           projectLogo={jornadasDocentes}
           projectName="Jornada Enseñanza Programacion"
           projectDescription="Un espacio de formación para compartir experiencias y discutir estrategias, metodologías y recursos para enseñar a programar en instituciones educativas."
-        />
-        <UqbarCard
-          link="https://docs.google.com/document/d/1hIyF7JHVLJ2u-a69tJRH5YfxPIdaiMDav3tCArkX-qc/view"
-          projectLogo={concursoWollokGame}
-          projectName="Concurso Wollok Game"
-          projectDescription="Si hiciste o estás haciendo un juego con Wollok Game, si estás aprendiendo a programar en objetos con Wollok y tenés ganas de explorar la interfaz gráfica de Game, si sos docente y queres mantenerte actualizado con las herramientas de Wollok, ¡este concurso es para vos! Junto con tu grupo, o individualmente, pueden hacer el juego que prefieran, presentarlo, que sea analizado por el jurado, que el público dé su opinión y ganar premios."
         />
       </div>
     </div>
