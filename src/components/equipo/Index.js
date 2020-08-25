26d708a50ef7f173eeb70dd2e772df97295246cd
PalumboN <nahuel.palumbo@gmail.com>
2020-08-25 16:03:26 -0300

    Guardando badge clicks

--- src/components/equipo/Index.js
+++ src/components/equipo/Index.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Badge, Button } from "@material-ui/core";
 import styles from "./Index.module.css";
 import miembros from './equipo.json';
 
 miembros.sort(() => 0.5 - Math.random()) // shuffle
 
-export function Miembro({ name, bio, email, badge, onClick, active }) {
+export function Miembro({ name, bio, email, badge, badgeData, addBadge, onClick, active }) {
   return (
     <div className={[styles.member, active ? styles.active : styles.inactive].join(" ")} onClick={onClick}>
       <div className={styles["photo"]}>
@@ -16,9 +16,9 @@ export function Miembro({ name, bio, email, badge, onClick, active }) {
         <div className={styles["bio"]}>{bio}</div>
         <div className={styles["bio"]}>{email}</div>
         {badge &&
-          <Badge badgeContent={4} color="primary" className={styles["badge"]}>
-            <Button variant="outlined" onClick={event => event.stopPropagation()}>
-              {badge?.text}
+          <Badge badgeContent={badgeData[badge.id]} color="primary" className={styles["badge"]}>
+            <Button variant="outlined" onClick={event => {event.stopPropagation(); addBadge(badge.id)}}>
+              {badge.text}
             </Button>
           </Badge>
         }
@@ -29,7 +29,36 @@ export function Miembro({ name, bio, email, badge, onClick, active }) {
 
 export default function Equipo() {
   const [active, setActive] = useState(null);
+  const [badgeData, setBadgeData] = useState({});
+
+  const BIN_ID = "5f455fdc4d8ce41113809ad1"
+  const API_URL = `https://api.jsonbin.io/b/${BIN_ID}`
+  
+  useEffect(() => {
+    fetch(API_URL, { headers: [["secret-key", "$2b$10$Q.HTJz84aOJcSRn55Toiqeto2Rd1Kp4xx3LC3cUbR6xxpjvTdyrs6"]] })
+      .then(response => response.json())
+      .then(setBadgeData)
+  }, [])
+
+  const addBadge = (id) => {
+    const currentValue = badgeData[id] || 0
+    const newBadgeData = { ...badgeData, [id]: currentValue + 1 }
+    console.log(newBadgeData)
+    setBadgeData(newBadgeData)
+    fetch(API_URL, { 
+      headers: [
+        ["content-type", "application/json"],
+        ["secret-key", "$2b$10$Q.HTJz84aOJcSRn55Toiqeto2Rd1Kp4xx3LC3cUbR6xxpjvTdyrs6"],
+        ["versioning", "false"]
+      ],
+      method: "PUT",
+      body: JSON.stringify(newBadgeData)
+    })
+
+  }
+
   const isActive = (index) => index === active
+
   return (
     <div className={styles["equipo"]} id="Equipo">
       <div className={styles["container"]}>
@@ -37,7 +66,7 @@ export default function Equipo() {
         <div className={styles["equipoListado"]}>
           {
             miembros.map((miembro, index) => (
-              <Miembro key={index} onClick={() => setActive(isActive(index) ? null : index)} active={isActive(index)} {...miembro} />
+              <Miembro key={index} onClick={() => setActive(isActive(index) ? null : index)} active={isActive(index)} badgeData={badgeData} addBadge={addBadge} {...miembro} />
             ))
           }
         </div>
