aaf9eb1f58bc99076c472f5679c50543c5f3bd9d
Nicolás Passerini <npasserini@gmail.com>
2020-09-20 21:35:17 -0300

    Primer prototipo de navbar

--- src/components/eventos/Index.js
+++ src/components/eventos/Index.js
@@ -9,9 +9,10 @@ import convocatoriaOct2020 from "./imgs/convocatoria-oct2020.jpg";
 export default function Eventos() {
   return (
     <div className={styles["eventos"]} id="Eventos">
-    <h2>Próximos</h2>
+      <h2>Próximos</h2>
       <div className={styles["eventosListado"]}>
         <UqbarCard
+          styles={{ minHeight: "493px" }}
           link="https://docs.google.com/document/d/e/2PACX-1vR0H_oeXpi7sVUwNQrdkwYqLOyY4stEBEAIkQlWEyCq7X_5lEHTSPZCgZUBdBFa1o4v3L5fivlhCz9q/pub"
           projectLogo={convocatoriaOct2020}
           projectName='Encuentros virtuales "Enseñando a programar" Octubre 2020'
@@ -21,6 +22,7 @@ export default function Eventos() {
 Una serie de encuentros virtuales para dialogar sobre las experiencias educativas en torno a la enseñanza de la Programación"
         />
         <UqbarCard
+          styles={{ minHeight: "493px" }}
           link="https://docs.google.com/document/d/e/2PACX-1vRcVrBm-xzDVPhR2CL1YsTK7xYRtT93KPSdisAaRq_KtERJjciRRl-cukJS0Sen87zVCSqflVD2DQRc/pub"
           projectLogo={convocatoriaDocente}
           projectName="Convocatoria Docente"
