65e26996350390ecce0eff2bf56513da68b37fa4
PalumboN <nahuel.palumbo@gmail.com>
2020-11-04 12:31:41 -0300

    Concurso Wollok Game 2020

--- src/components/eventos/Index.js
+++ src/components/eventos/Index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Index.module.css";
 import UqbarCard from "../utils/UqbarCard.js"
-import concursoWollokGame from "./imgs/concurso-wollok-game.jpg";
+import concursoWollokGame2019 from "./imgs/concurso-wollok-game-2019.jpg";
 import jornadasDocentes from "./imgs/jornadas-docentes.jpg";
 import convocatoriaDocente from "./imgs/convocatoria-docente.jpeg";
 import convocatoriaOct2020 from "./imgs/encuentro-virtual-segundo-encuentro.jpg";
+import concursoWollokGame2020 from "./imgs/concurso-wollok-game-2020.jpg";
 
 export default function Eventos() {
   return (
@@ -12,9 +13,15 @@ export default function Eventos() {
       <h2>Próximos</h2>
       <div className={styles["eventosListado"]}>
         <UqbarCard
+          link="https://docs.google.com/document/d/1mpsdbLlw2Y5r6hCcxQilL3LFHzuXUZ16xmAOGT8F9-w/view"
+          projectLogo={concursoWollokGame2020}
+          projectName="Concurso Wollok Game"
+          projectDescription="Si hiciste o estás haciendo un juego con Wollok Game, si estás aprendiendo a programar en objetos con Wollok y tenés ganas de explorar la interfaz gráfica de Game, si sos docente y queres mantenerte actualizado con las herramientas de Wollok, ¡este concurso es para vos! Junto con tu grupo, o individualmente, pueden hacer el juego que prefieran, presentarlo, que sea analizado por el jurado, que el público dé su opinión y ganar premios."
+        />
+        <UqbarCard
           link="https://docs.google.com/document/d/e/2PACX-1vR0H_oeXpi7sVUwNQrdkwYqLOyY4stEBEAIkQlWEyCq7X_5lEHTSPZCgZUBdBFa1o4v3L5fivlhCz9q/pub"
           projectLogo={convocatoriaOct2020}
-          projectName='Encuentros virtuales "Enseñando a programar" Octubre 2020'
+          projectName='Encuentros virtuales "Enseñando a programar" 2020'
           projectDescription="¿Qué estrategias usas para enseñar a programar?
 ¿Con qué problemas te encontrás?
 ¿Qué herramientas te son útiles?
@@ -31,8 +38,8 @@ Una serie de encuentros virtuales para dialogar sobre las experiencias educativa
       <div className={styles["eventosListado"]}>
         <UqbarCard
           link="https://docs.google.com/document/d/1hIyF7JHVLJ2u-a69tJRH5YfxPIdaiMDav3tCArkX-qc/view"
-          projectLogo={concursoWollokGame}
-          projectName="Concurso Wollok Game"
+          projectLogo={concursoWollokGame2019}
+          projectName="Concurso Wollok Game 2019"
           projectDescription="Si hiciste o estás haciendo un juego con Wollok Game, si estás aprendiendo a programar en objetos con Wollok y tenés ganas de explorar la interfaz gráfica de Game, si sos docente y queres mantenerte actualizado con las herramientas de Wollok, ¡este concurso es para vos! Junto con tu grupo, o individualmente, pueden hacer el juego que prefieran, presentarlo, que sea analizado por el jurado, que el público dé su opinión y ganar premios."
         />
         <UqbarCard
