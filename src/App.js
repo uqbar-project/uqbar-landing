59fe856fe1b9ec2fa8caa5e017b76d8f54b88b14
PalumboN <nahuel.palumbo@gmail.com>
2021-03-05 16:29:30 -0300

    Hola Talleres

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header/Index.js";
 import Inicio from "./components/inicio/Index.js";
 import Donaciones from "./components/donaciones/Index.js";
 import Proyectos from "./components/proyectos/Index.js";
+import Talleres from "./components/talleres/Index.js";
 import Equipo from "./components/equipo/Index.js";
 import Contacto from "./components/contacto/Index.js";
 import Eventos from "./components/eventos/Index.js";
@@ -24,6 +25,7 @@ function App() {
           </Route>
           <Route path="/">
             <Inicio/>
+            <Talleres/>
             <Proyectos/>
             <Equipo/>
             <Donaciones/>
---
+++ src/components/talleres/Index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import styles from "./Index.module.css"
+import UqbarCard from "../utils/UqbarCard.js"
+import ludover from "./ludover.png"
+import maquinasVirtuales from "./maquinas-virtuales.jpg"
+import talleresBanner from "./uqbar-talleres.jpeg"
+
+export default function Proyectos() {
+  return (
+    <div className="container" id="Talleres">
+      {/* TODO: <ImgLink>? */}
+      <a href="https://talleres.uqbar.org/" target="_blank" rel="noopener noreferrer">
+        <img src={talleresBanner} alt="Talleres Uqbar" />
+      </a>
+      <p className={styles["description"]}>Desde la Fundación, durante la primera parte del año 2021 se dictarán dos talleres gratuitos: <strong>Ludover (taller de videojuegos)</strong> y <strong>Máquinas Virtuales</strong>.</p>
+      <div className={styles["proyectosListado"]}>
+        <UqbarCard
+          link="https://talleres.uqbar.org/"
+          projectLogo={ludover}
+          projectName="Ludover"
+          projectDescription="Ludover es un espacio para facilitar el acercamiento de personas que deseen desarrollar videojuegos, y ayudarlas a llevar adelante sus proyectos, desde la concepción de la idea hasta la publicación, o el objetivo que se quiera lograr."
+        />
+        <UqbarCard
+          link="https://talleres.uqbar.org/"
+          projectLogo={maquinasVirtuales}
+          projectName="Máquinas Virtuales"
+          projectDescription="Taller de máquinas virtuales es un espacio para aprender sobre el funcionamiento de la implementación de lenguajes de programación modernos. El objetivo principal del taller es aprender haciendo, y trabajar sobre técnicas de implementación y optimización."
+        />
+      </div>
+    </div>
+  )
+}
