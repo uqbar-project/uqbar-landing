0e3d55dfb1826aa74656ec8d00a681fc30fe511f
Nicolás Passerini <npasserini@gmail.com>
2022-11-16 13:39:47 -0300

    Reordenar menú

--- src/components/header/Index.js
+++ src/components/header/Index.js
@@ -6,56 +6,55 @@ import { HashLink as Link } from "react-router-hash-link";
 
 export default function Header() {
   return (
-      <div className={styles["header"]}>
-        <div className="container">
-          <div className={styles["uqbarlogo"]}>
-            <Link to="/">
-              <img src={logo} alt="Uqbar"/>
-            </Link>
-          </div>
-          <div className={styles["navbar"]}>
-            <ul>
-              <li>
-                <Link to="/#Proyectos">
-                  Proyectos
-                </Link>
-              </li>
-              <li>
-                <Link to="/#Equipo">
-                  Equipo
-                </Link>
-              </li>
-              <li>
-                <Link to="/#Donaciones">
-                  Donaciones
-                </Link>
-              </li>
-              <li>
-                <Link to="/#Contacto">
-                  Contacto
-                </Link>
-              </li>
-              <li>
-                <Link to="/ideas">
-                    Ideas
-                </Link>
-              </li>
-              <li>
-                <Link to="/eventos">
-                    Eventos
-                </Link>
-              </li>
-              
-              <li>
-                <a href="https://discord.gg/5wVEwse">
-                  Únete a Nuestro Discord! 
-                  <img src={discord} alt="Discord" className={styles["discordlogo"]}/>
-                </a>
-              </li>
-            </ul>
-          </div>
+    <div className={styles["header"]}>
+      <div className="container">
+        <div className={styles["uqbarlogo"]}>
+          <Link to="/">
+            <img src={logo} alt="Uqbar" />
+          </Link>
+        </div>
+        <div className={styles["navbar"]}>
+          <ul>
+            <li>
+              <Link to="/#Proyectos">
+                Proyectos
+              </Link>
+            </li>
+            <li>
+              <Link to="/#Equipo">
+                Equipo
+              </Link>
+            </li>
+            <li>
+              <Link to="/#Donaciones">
+                Donaciones
+              </Link>
+            </li>
+            <li>
+              <Link to="/eventos">
+                Eventos
+              </Link>
+            </li>
+            <li>
+              <Link to="/ideas">
+                Ideas
+              </Link>
+            </li>
+            <li>
+              <Link to="/#Contacto">
+                Contacto
+              </Link>
+            </li>
+            <li>
+              <a href="https://discord.gg/5wVEwse">
+                Únete a Nuestro Discord!
+                <img src={discord} alt="Discord" className={styles["discordlogo"]} />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
+    </div>
   );
 }
 
