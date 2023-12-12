fc67c0a7e308d11e37cba11a970e900902265f14
palumbon <nahuel.palumbo@gmail.com>
2023-12-12 23:20:17 +0100

    Concurso Wollok Game 2023

--- src/components/eventos/Index.js
+++ src/components/eventos/Index.js
@@ -5,6 +5,7 @@ import concursoWollokGame2019 from "./imgs/concurso-wollok-game-2019.jpg";
 import concursoWollokGame2020 from "./imgs/votacion-concurso-wollok-game-2020.jpg";
 import concursoWollokGame2021 from "./imgs/concurso-wollok-game-2021.png";
 import concursoWollokGame2022 from "./imgs/concurso-wollok-game-2022.png";
+import concursoWollokGame2023 from "./imgs/concurso-wollok-game-2023.png";
 import jornadasDocentes from "./imgs/jornadas-docentes.jpg";
 import convocatoriaDocente from "./imgs/convocatoria-docente.jpeg";
 import encuentroVirtual from "./imgs/encuentro-virtual.jpg";
@@ -19,19 +20,10 @@ export function EventosActuales() {
   return (
     <UqbarCardGrid>
       <UqbarCard
-        link="https://utn.zoom.us/j/87016722899"
-        projectLogo={uqbarLogo}
-        projectName="¡Cierre del año con Uqbar!"
-        projectDescription="Martes 27 de Diciembre: Les invitamos a cerrar del año con nosotrxs. 
-        Haremos un repaso de todas las cosas que se hicieron en Uqbar durante el año, y también presentaremos lxs ganadorxs del concurso Wollok Game 2022. La reunión será virtual en el link que se encuentra en la descripción de este evento. 
-        ¡Lxs esperamos para brindar por otro año en comunidad!"
-      />
-
-      <UqbarCard
-        link="https://docs.google.com/document/d/e/2PACX-1vQUxRzRAH_dUPMIO-N39x8F_mHDTa_DJb9ENgSxoEn4Ezb09PdzGr6pyD4UUv9Nm5-n1oVVBFD2B9Pu/pub"
-        projectLogo={concursoWollokGame2022}
-        projectName="Concurso Wollok Game"
-        projectDescription="Si hiciste o estás haciendo un juego con Wollok Game, si estás aprendiendo a programar en objetos con Wollok y tenés ganas de explorar la interfaz gráfica de Game, si sos docente y querés mantenerte actualizado con las herramientas de Wollok, ¡este concurso es para vos! Junto con tu grupo, o individualmente, pueden hacer el juego que prefieran, presentarlo, que sea analizado por el jurado y que el público dé su opinión."
+        link="https://docs.google.com/document/d/e/2PACX-1vQOapZd5M2xEoq6RR9PiHCynvIJ0A2SRlP_1wTy2I2-D7go7jSan8dAjTBs3C74SmKH-y80jnBoWpM3/pub"
+        projectLogo={concursoWollokGame2023}
+        projectName="Concurso Wollok Game 2023"
+        projectDescription="¡Se hizo desear pero llegó el Concurso de Wollok Game 5ta edición! Así que ya sabés, si hiciste o estás haciendo un juego con Wollok Game, si estás aprendiendo a programar en objetos con Wollok y tenés ganas de explorar la interfaz gráfica de Game, si sos docente y querés mantenerte actualizado con las herramientas de Wollok, ¡este concurso es para vos! Junto con tu grupo, o individualmente, pueden hacer el juego que prefieran, presentarlo, que sea analizado por el jurado y que el público dé su opinión."
       />
     </UqbarCardGrid>
   )
@@ -45,6 +37,22 @@ export default function Eventos() {
 
       <h2>2022</h2>
       <UqbarCardGrid>
+        <UqbarCard
+          link="https://utn.zoom.us/j/87016722899"
+          projectLogo={uqbarLogo}
+          projectName="¡Cierre del año con Uqbar!"
+          projectDescription="Martes 27 de Diciembre: Les invitamos a cerrar del año con nosotrxs. 
+        Haremos un repaso de todas las cosas que se hicieron en Uqbar durante el año, y también presentaremos lxs ganadorxs del concurso Wollok Game 2022. La reunión será virtual en el link que se encuentra en la descripción de este evento. 
+        ¡Lxs esperamos para brindar por otro año en comunidad!"
+        />
+
+        <UqbarCard
+          link="https://docs.google.com/document/d/e/2PACX-1vQUxRzRAH_dUPMIO-N39x8F_mHDTa_DJb9ENgSxoEn4Ezb09PdzGr6pyD4UUv9Nm5-n1oVVBFD2B9Pu/pub"
+          projectLogo={concursoWollokGame2022}
+          projectName="Concurso Wollok Game"
+          projectDescription="Si hiciste o estás haciendo un juego con Wollok Game, si estás aprendiendo a programar en objetos con Wollok y tenés ganas de explorar la interfaz gráfica de Game, si sos docente y querés mantenerte actualizado con las herramientas de Wollok, ¡este concurso es para vos! Junto con tu grupo, o individualmente, pueden hacer el juego que prefieran, presentarlo, que sea analizado por el jurado y que el público dé su opinión."
+        />
+
         <UqbarCard
           link="https://www.instagram.com/p/ClHPR0vPE-7/?utm_source=ig_web_copy_link"
           projectLogo={unahurTic}
@@ -62,6 +70,7 @@ export default function Eventos() {
           projectName="Proyectos Innovadores con Software Libre"
           projectDescription="Aunque a veces es poco visible para el gran público por fuera de los especialistas informáticos, el software libre es un pilar fundamental para el funcionamiento de muchos de los recursos tecnológicos que usamos a diario, así como de los proyectos más innovadores y creativos. En esta jornada contaremos tres experiencias concretas de utilización de software libre para aplicaciones de alta demanda y complejidad, repasando las ventajas y los desafíos de haber encarado los diferentes proyectos con esa estrategia, así como los retos y objetivos de cara al futuro."
         />
+
         <UqbarCard
           link="https://docs.google.com/document/d/1FTdlT7WH3EJIC68BYJRkXeLhPQYAzg27Ya4t3RylOKs/edit?usp=sharing"
           projectLogo={maquinasVirtuales2022}
